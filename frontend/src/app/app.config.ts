cedcd3117c12d73e01ffc2e64b6d31bfad1b1a2b
ashishgoplani <ashish.goplani@gantnerindia.com>
2025-02-11 12:11:09 +0530

    frontend wd backend

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -6,11 +6,11 @@ import { provideClientHydration, withEventReplay } from '@angular/platform-brows
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import { providePrimeNG } from 'primeng/config';
 import Aura from '@primeng/themes/aura';
-import { provideHttpClient } from '@angular/common/http';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),provideAnimationsAsync(),  provideHttpClient(),
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(withEventReplay()),provideAnimationsAsync(),  provideHttpClient(withFetch()),
     providePrimeNG({
         theme: {
             preset: Aura
